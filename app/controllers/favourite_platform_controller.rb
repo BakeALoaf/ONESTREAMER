f5556a20f8324b7d9ef452b2115931d45dccd166
andre teixeira <andre.gomes.teixeira22@gmail.com>
2022-12-07 16:28:13 +0000

    platform icons

--- app/controllers/favourite_platform_controller.rb
+++ app/controllers/favourite_platform_controller.rb
@@ -2,4 +2,6 @@ class FavouritePlatformController < ApplicationController
   def index
     @user = current_user
   end
+  def create
+  end
 end
--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,5 +1,7 @@
 class PlatformsController < ApplicationController
   def index
     @platforms = Platform.all
+    @favourite_platform = FavouritePlatform.new
+    @user = current_user
   end
 end
