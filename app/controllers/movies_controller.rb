dc38e67adc25efa406cf24cfbcc652ad539645d2
James Barker <jbarkerlouth@gmail.com>
2022-12-13 11:36:01 +0100

    filtered search and random carousel

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,30 @@
 class MoviesController < ApplicationController
   def index
-    # @top_movies = Movie.all.sample(4)
     @favourite_movie = FavouriteMovie.new
     @favourite_platforms = current_user.platforms
     @random_movies = Movie.all.sample(4)
 
     if params[:query].present?
-      @movies = Movie.where("name ILIKE ?", "%#{params[:query]}%")
+      if @favourite_platforms.length > 0
+        @movies = []
+        sql_query = <<~SQL
+          movies.name ILIKE :query
+          AND platform ILIKE :favourite_name
+        SQL
+        @favourite_platforms.each do |platform|
+          @movies << Movie.where(sql_query, query: "%#{params[:query]}%", favourite_name: "%#{platform.name}")
+        end
+        @movies.flatten!
+      else
+        @movies = Movie.where("name ILIKE ?", "%#{params[:query]}%")
+      end
     elsif @favourite_platforms.length > 0
       @movies = []
       @favourite_platforms.each do |platform|
         @movies << platform.movies
       end
       @movies.flatten!
-    elsif params[:query].present?
-      @movies = Movie.where("name ILIKE ?", "%#{params[:query]}%")
+      @random_movies = @movies.sample(4)
     else
       @platforms = Platform.all
       @movies = Movie.all
