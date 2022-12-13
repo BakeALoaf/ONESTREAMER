a2280a77b3fe8c2c6c6632a596dda05d762be9ca
Andreas Arnsel <arnsel@pm.me>
2022-12-13 15:00:00 +0100

    did some ajax stuff

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,12 +1,19 @@
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
-// // Connects to data-controller="search"
-// export default class extends Controller {
-//   connect() {
-//     const searchbar = document.getElementById("search_icon");
-//     search_icon.addEventListener("click", (event) => {
-//       console.log(event);
-//       console.log(event.currentTarget);
-//     });
-//   }
-// }
+// Connects to data-controller="search"
+export default class extends Controller {
+  static targets = [ "form", "list", "searchInput" ]
+
+  connect() {
+  }
+
+  update() {
+  const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
+  fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      console.log(data);
+      this.listTarget.outerHTML = data;
+    })
+  }
+}
